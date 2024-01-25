35ca56b81514bc420de46f23178370e96e9d0c96
Ogaraga <raymond.ogaraga@gmail.com>
2024-01-25 15:26:09 +0100

    review

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,12 @@ const PORT = process.env.PORT || 3000
 
 const app = express();
 app.use(cors({origin: "http://localhost:5173"}))
+app.use(cors({origin:"https://crude-ogaraga.vercel.app/"}))
 app.use(express.json());
 app.use('/', router);
 mongoose.connect(process.env.Base_URL).then(()=>console.log('connection to database granted')).catch(()=>console.error('Conection to database denied'));
 
 app.listen(PORT, ()=> console.log(`Server started on ${PORT}`));
-
-
 
 
 
